f0946f251abae173dbb15c2ff0948e2e04cd9f76
Haru-22-haru <haroldralph22@gmail.com>
2024-01-12 16:58:09 +0800

    added surgery, vaxx, diagnosis, on dropdown

--- resources/views/admin/petrecords.blade.php
+++ resources/views/admin/petrecords.blade.php
@@ -183,6 +183,37 @@
 											</svg>
 										</button>
 										<div class="dropdown-menu more_button" data-bs-popper="none">
+										<button class="dropdown-item add_diagnosis-action" id="add_diagnosis">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+													<g clip-path="url(#clip0_6230_1794)">
+														<path d="M10 12C10 12.5304 10.2107 13.0391 10.5858 13.4142C10.9609 13.7893 11.4696 14 12 14C12.5304 14 13.0391 13.7893 13.4142 13.4142C13.7893 13.0391 14 12.5304 14 12C14 11.4696 13.7893 10.9609 13.4142 10.5858C13.0391 10.2107 12.5304 10 12 10C11.4696 10 10.9609 10.2107 10.5858 10.5858C10.2107 10.9609 10 11.4696 10 12Z" stroke="#1C1C1C" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+														<path d="M21 12C18.6 16 15.6 18 12 18C8.4 18 5.4 16 3 12C5.4 8 8.4 6 12 6C15.6 6 18.6 8 21 12Z" stroke="#1C1C1C" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </g>
+													<defs>
+														<clipPath id="clip0_6230_1794">
+															<rect width="24" height="24" fill="white" /> </clipPath>
+													</defs>
+												</svg>Add Diagnosis
+											</button>
+											<button class="dropdown-item add_immunization_action" id="add_immunization">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+													<g clip-path="url(#clip0_6230_538)">
+														<path d="M13.5 6.5L17.5 10.5M4 20.0001H8L18.5 9.50006C19.0304 8.96963 19.3284 8.2502 19.3284 7.50006C19.3284 6.74991 19.0304 6.03049 18.5 5.50006C17.9696 4.96963 17.2501 4.67163 16.5 4.67163C15.7499 4.67163 15.0304 4.96963 14.5 5.50006L4 16.0001V20.0001Z" stroke="#1C1C1C" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </g>
+													<defs>
+														<clipPath id="clip0_6230_538">
+															<rect width="24" height="24" fill="white" /> </clipPath>
+													</defs>
+												</svg>Add Vaccine</button>
+											<button class="dropdown-item add_surgery_action" id="add_surgery">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+													<g clip-path="url(#clip0_6230_1803)">
+														<path d="M19 8C19.5304 8 20.0391 7.78929 20.4142 7.41421C20.7893 7.03914 21 6.53043 21 6C21 5.46957 20.7893 4.96086 20.4142 4.58579C20.0391 4.21071 19.5304 4 19 4H5C4.46957 4 3.96086 4.21071 3.58579 4.58579C3.21071 4.96086 3 5.46957 3 6C3 6.53043 3.21071 7.03914 3.58579 7.41421C3.96086 7.78929 4.46957 8 5 8M19 8H5M19 8V18C19 18.5304 18.7893 19.0391 18.4142 19.4142C18.0391 19.7893 17.5304 20 17 20H7C6.46957 20 5.96086 19.7893 5.58579 19.4142C5.21071 19.0391 5 18.5304 5 18V8M10 12H14" stroke="#1C1C1C" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </g>
+													<defs>
+														<clipPath id="clip0_6230_1803">
+															<rect width="24" height="24" fill="white" /> </clipPath>
+													</defs>
+												</svg>Add Surgery</button>
+
+											<hr />
 											<button class="dropdown-item View_pet-action" id="View" data-container-id="{{ $petrecord->id }}">
 												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 													<g clip-path="url(#clip0_6230_1794)">
