d604bb65208c467662b10412008e2e2baa2ffc4a
Haru-22-haru <haroldralph22@gmail.com>
2024-02-20 22:07:07 +0800

    added discard and save success modal

--- resources/views/admin/admin_settings.blade.php
+++ resources/views/admin/admin_settings.blade.php
@@ -317,6 +317,65 @@
 			</div>
 		</div>
 	</div>
+
+  <div class="modal fade show" role="dialog" tabindex="-1" id="save_changes_success">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content success_modal">
+                <div class="modal-header success_header">
+                    <div class="success_icon_container"><span class="success_icon"><svg
+                                xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+                                viewBox="0 0 32 32" fill="none">
+                                <g clip-path="url(#clip0_5693_10654)">
+                                    <path
+                                        d="M12 16L14.6667 18.6667L20 13.3333M4 16C4 17.5759 4.31039 19.1363 4.91345 20.5922C5.5165 22.0481 6.40042 23.371 7.51472 24.4853C8.62902 25.5996 9.95189 26.4835 11.4078 27.0866C12.8637 27.6896 14.4241 28 16 28C17.5759 28 19.1363 27.6896 20.5922 27.0866C22.0481 26.4835 23.371 25.5996 24.4853 24.4853C25.5996 23.371 26.4835 22.0481 27.0866 20.5922C27.6896 19.1363 28 17.5759 28 16C28 14.4241 27.6896 12.8637 27.0866 11.4078C26.4835 9.95189 25.5996 8.62902 24.4853 7.51472C23.371 6.40042 22.0481 5.5165 20.5922 4.91345C19.1363 4.31039 17.5759 4 16 4C14.4241 4 12.8637 4.31039 11.4078 4.91345C9.95189 5.5165 8.62902 6.40042 7.51472 7.51472C6.40042 8.62902 5.5165 9.95189 4.91345 11.4078C4.31039 12.8637 4 14.4241 4 16Z"
+                                        stroke="#5BB85A" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_5693_10654">
+                                        <rect width="32" height="32" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg></span></div>
+                </div>
+                <div class="modal-body success_message">
+                    <div>
+                        <h1>Success!</h1>
+                        <p>Your changes have been successfully saved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  <div class="modal fade show" role="dialog" tabindex="-1" id="edit_discard">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content discard_modal">
+				<div class="modal-header discard_header">
+					<div class="discard_icon_container"><span class="discard_icon"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+  <g clip-path="url(#clip0_5693_21111)">
+    <path d="M13.3333 13.3333L18.6667 18.6667M18.6667 13.3333L13.3333 18.6667M4 16C4 17.5759 4.31039 19.1363 4.91345 20.5922C5.5165 22.0481 6.40042 23.371 7.51472 24.4853C8.62902 25.5996 9.95189 26.4835 11.4078 27.0866C12.8637 27.6896 14.4241 28 16 28C17.5759 28 19.1363 27.6896 20.5922 27.0866C22.0481 26.4835 23.371 25.5996 24.4853 24.4853C25.5996 23.371 26.4835 22.0481 27.0866 20.5922C27.6896 19.1363 28 17.5759 28 16C28 14.4241 27.6896 12.8637 27.0866 11.4078C26.4835 9.95189 25.5996 8.62902 24.4853 7.51472C23.371 6.40042 22.0481 5.5165 20.5922 4.91345C19.1363 4.31039 17.5759 4 16 4C14.4241 4 12.8637 4.31039 11.4078 4.91345C9.95189 5.5165 8.62902 6.40042 7.51472 7.51472C6.40042 8.62902 5.5165 9.95189 4.91345 11.4078C4.31039 12.8637 4 14.4241 4 16Z" stroke="#DA534F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </g>
+  <defs>
+    <clipPath id="clip0_5693_21111">
+      <rect width="32" height="32" fill="white"/>
+    </clipPath>
+  </defs>
+</svg></span></div>
+				</div>
+				<div class="modal-body discard_message">
+					<div>
+						<h1>Discard changes?</h1>
+						<p>Any unsaved progress will be lost.</p>
+					</div>
+				</div>
+				<div class="modal-footer discard_footer">
+					<button class="btn return_btn" data-bs-dismiss="modal" id="return_btn" type="button"><span class="return_btn_base">Return</span></button>
+					<button class="btn discard_btn" id="discard_btn" type="button" data-bs-dismiss="modal"><span class="discard_btn_base">Discard</span></button>
+				</div>
+			</div>
+		</div>
+	</div>
 	@if (session('success'))
 	<script>
         $(document).ready(function() {
@@ -334,6 +393,27 @@
 	<script src="{{ asset('assets/js/sidebartry.js') }}"></script>
 
 	<script>
+
+var edit_save_changes = document.getElementById('edit_save_changes');
+var save_changes_success = new bootstrap.Modal(document.getElementById('save_changes_success'));
+
+edit_save_changes.addEventListener('click', function(){
+  save_changes_success.show();
+  setTimeout(function () {
+    save_changes_success.hide(); // Use Bootstrap modal's hide() method
+  }, 2000);
+});
+
+
+        var cancel_pass_btn = document.getElementById('cancel_pass_btn');
+        var edit_discard = new bootstrap.Modal(document.getElementById('edit_discard'));
+
+        cancel_pass_btn.addEventListener('click', function(){
+          edit_discard.show();
+        });
+        
+
+
 		// Get references to the elements
 		const passAndSecurityBox = document.getElementById('pass_and_security_box');
       const changePasswordDiv = document.getElementById('change_password');
