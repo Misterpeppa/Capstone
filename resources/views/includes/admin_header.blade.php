6e7c97599428deb8529ee77c8166d4ac7411932c
Haru-22-haru <haroldralph22@gmail.com>
2024-02-23 09:52:00 +0800

    nagpalit lang ng icon, pakopya

--- resources/views/includes/admin_header.blade.php
+++ resources/views/includes/admin_header.blade.php
@@ -63,14 +63,17 @@
 					</svg> </a>
 					<a class="sidebar_link" href="{{ route('admin_client') }}">Clients</a></li>
 				<li class="list-inline-item {{ $activeTab === 'inventory' ? 'active' : '' }}">
-        <a href="{{ route('admin_inv') }}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-						<g clip-path="url(#clip0_5274_11651)">
-							<path d="M20 7.5L12 3L4 7.5M20 7.5V16.5L12 21M20 7.5L12 12M12 21L4 16.5V7.5M12 21V12M4 7.5L12 12M8.2002 9.79995L15.8002 5.19995" stroke="#004D53" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </g>
-						<defs>
-							<clipPath id="clip0_5274_11651">
-								<rect width="24" height="24" fill="white" /> </clipPath>
-						</defs>
-					</svg></a>
+        <a href="{{ route('admin_inv') }}"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-box-seam" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 3l8 4.5v9l-8 4.5l-8 -4.5v-9l8 -4.5" />
+  <path d="M12 12l8 -4.5" />
+  <path d="M8.2 9.8l7.6 -4.6" />
+  <path d="M12 12v9" />
+  <path d="M12 12l-8 -4.5" />
+</svg>
+
+
+</a>
 					<a class="sidebar_link" href="{{ route('admin_inv') }}">Inventory</a></li>
 			</ul>
 			<hr class="line_separator">
